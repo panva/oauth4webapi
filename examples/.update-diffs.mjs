3b3f810fa57d048856677c8c1ff640c32f535b9f
Filip Skokan <panva.ip@gmail.com>
2022-03-16 21:31:39 +0100

    ci: check diffs formatting in CI

--- examples/.update-diffs.mjs
+++ examples/.update-diffs.mjs
@@ -9,4 +9,3 @@ const diffable = [
 ]
 
 await Promise.all(diffable.map(([from, to]) => $`git diff HEAD:examples/${from}.ts examples/${to}.ts > examples/${to}.diff`))
-// TODO: exit 0 or 1 depending on updated or not and run update-diffs in CI format job
