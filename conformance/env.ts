b3f970ca9ca5662f7f76f71f95dfe4601d1b47f8
Filip Skokan <panva.ip@gmail.com>
2022-06-09 13:49:55 +0200

    test: default conformance alg to PS for FAPI

--- conformance/env.ts
+++ conformance/env.ts
@@ -1,7 +1,9 @@
-const {
-  JWS_ALGORITHM = 'RS256',
-  PLAN_NAME = 'oidcc-client-basic-certification-test-plan',
-  VARIANT = '{}',
-} = process.env
+const { PLAN_NAME = 'oidcc-client-basic-certification-test-plan', VARIANT = '{}' } = process.env
 
-export { JWS_ALGORITHM, PLAN_NAME, VARIANT }
+export const JWS_ALGORITHM = process.env.JWS_ALGORITHM
+  ? process.env.JWS_ALGORITHM
+  : PLAN_NAME.startsWith('fapi')
+  ? 'PS256'
+  : 'RS256'
+
+export { PLAN_NAME, VARIANT }
