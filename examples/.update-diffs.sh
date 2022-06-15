5ac42b8bcc77e2a2a0ec23d22419a6511f053d0f
Filip Skokan <panva.ip@gmail.com>
2022-06-15 10:55:56 +0200

    chore: upgrade dev deps

---
+++ examples/.update-diffs.sh
@@ -0,0 +1,5 @@
+git diff HEAD:examples/code.ts examples/dpop.ts > examples/dpop.diff
+git diff HEAD:examples/code.ts examples/par.ts > examples/par.diff
+git diff HEAD:examples/code.ts examples/private_key_jwt.ts > examples/private_key_jwt.diff
+git diff HEAD:examples/code.ts examples/public.ts > examples/public.diff
+git diff HEAD:examples/fapi2.ts examples/fapi2-message-signing.ts > examples/fapi2-message-signing.diff
