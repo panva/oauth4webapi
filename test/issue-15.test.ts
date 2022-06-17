ac6417afa5d6eb9d9a7a9d1436a6a6bff64ec22c
Filip Skokan <panva.ip@gmail.com>
2022-06-17 14:19:43 +0200

    test: add tests for #15

---
+++ test/issue-15.test.ts
@@ -0,0 +1,20 @@
+// see https://github.com/panva/oauth4webapi/issues/15
+
+import test from 'ava'
+import { issuer, client, getResponse } from './_setup.js'
+import * as lib from '../src/index.js'
+
+const response = getResponse(
+  JSON.stringify({ access_token: 'foo', token_type: 'bearer', scope: '' }),
+)
+
+test('handles empty scope from the Token Endpoint', async (t) => {
+  await t.notThrowsAsync(() =>
+    Promise.all([
+      lib.processAuthorizationCodeOAuth2Response(issuer, client, response),
+      lib.processDeviceCodeResponse(issuer, client, response),
+      lib.processClientCredentialsResponse(issuer, client, response),
+      lib.processRefreshTokenResponse(issuer, client, response),
+    ]),
+  )
+})
