db7d62b00e7f432325c24d2c16944b9d7d8621e3
Filip Skokan <panva.ip@gmail.com>
2022-08-18 16:02:24 +0200

    chore: upgrade dev deps

--- .mdn_links.cjs
+++ .mdn_links.cjs
@@ -9,7 +9,7 @@ const doms = new Set([
 ])
 
 module.exports.load = function load(app) {
-  app.renderer.addUnknownSymbolResolver('@typescript/lib-dom', (name) => {
+  app.renderer.addUnknownSymbolResolver('typescript', (name) => {
     if (doms.has(name)) {
       return `https://developer.mozilla.org/en-US/docs/Web/API/${name}`
     }
