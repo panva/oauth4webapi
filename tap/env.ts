1e2f4f429bf05b181a2d25c4f9de7d529616ca3b
Filip Skokan <panva.ip@gmail.com>
2022-10-28 21:09:26 +0200

    test: tap recognize bun

--- tap/env.ts
+++ tap/env.ts
@@ -1,7 +1,9 @@
 // @ts-expect-error
+export const isBun = typeof Bun !== 'undefined'
+// @ts-expect-error
 export const isDeno = typeof Deno !== 'undefined'
 // @ts-expect-error
-export const isNode = typeof process !== 'undefined'
+export const isNode = !isBun && typeof process !== 'undefined'
 // @ts-expect-error
 export const isEdgeRuntime = typeof EdgeRuntime !== 'undefined'
 export const isBrowser =
