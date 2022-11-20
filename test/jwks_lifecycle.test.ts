22d8894359a151e52da2202ecb5457b1480354ac
Filip Skokan <panva.ip@gmail.com>
2022-11-20 10:42:39 +0100

    test(tap): use jarm for jwks lifecycle tests

--- test/jwks_lifecycle.test.ts
+++ test/jwks_lifecycle.test.ts
@@ -1,5 +1,5 @@
 import anyTest, { type TestFn } from 'ava'
-import setup, { type Context, teardown, issuer, getResponse, endpoint, client } from './_setup.js'
+import setup, { type Context, teardown, issuer, endpoint, client } from './_setup.js'
 import * as lib from '../src/index.js'
 import * as jose from 'jose'
 import timekeeper from 'timekeeper'
@@ -27,27 +27,30 @@ test.serial('jwks_uri force refetch', async (t) => {
   const as: lib.AuthorizationServer = {
     ...issuer,
     jwks_uri: endpoint('jwks-force-refetch'),
-    userinfo_signing_alg_values_supported: ['ES256'],
+    authorization_signing_alg_values_supported: ['ES256'],
   }
   const c: lib.Client = {
     ...client,
   }
 
-  let jwt = await new jose.SignJWT({ sub: 'subject' })
+  let jwt = await new jose.SignJWT({})
     .setProtectedHeader({ alg: 'ES256' })
+    .setAudience(c.client_id)
+    .setExpirationTime('5m')
+    .setIssuer(as.issuer)
     .sign(key.privateKey)
 
-  let response = getResponse(jwt, { headers: new Headers({ 'content-type': 'application/jwt' }) })
+  let params = new URLSearchParams({ response: jwt })
 
-  await lib.processUserInfoResponse(as, c, lib.skipSubjectCheck, response)
-  await lib.processUserInfoResponse(as, c, lib.skipSubjectCheck, response)
+  await lib.validateJwtAuthResponse(as, c, params)
+  await lib.validateJwtAuthResponse(as, c, params)
 
   timekeeper.travel(now + 299 * 1000)
-  await lib.processUserInfoResponse(as, c, lib.skipSubjectCheck, response)
+  await lib.validateJwtAuthResponse(as, c, params)
   timekeeper.travel(now + 300 * 1000)
 
   // re-fetch forced
-  await t.throwsAsync(() => lib.processUserInfoResponse(as, c, lib.skipSubjectCheck, response))
+  await t.throwsAsync(() => lib.validateJwtAuthResponse(as, c, params))
 })
 
 test.serial('jwks_uri refetch if off cooldown and needed', async (t) => {
@@ -73,25 +76,31 @@ test.serial('jwks_uri refetch if off cooldown and needed', async (t) => {
   const as: lib.AuthorizationServer = {
     ...issuer,
     jwks_uri: endpoint('jwks-refetch-off-cooldown'),
-    userinfo_signing_alg_values_supported: ['ES256'],
+    authorization_signing_alg_values_supported: ['ES256'],
   }
   const c: lib.Client = {
     ...client,
   }
 
-  let jwt = await new jose.SignJWT({ sub: 'subject' })
+  let jwt = await new jose.SignJWT({})
     .setProtectedHeader({ alg: 'ES256', kid: 'foo' })
+    .setAudience(c.client_id)
+    .setExpirationTime('5m')
+    .setIssuer(as.issuer)
     .sign(key.privateKey)
 
-  let response = getResponse(jwt, { headers: new Headers({ 'content-type': 'application/jwt' }) })
+  let params = new URLSearchParams({ response: jwt })
 
-  await lib.processUserInfoResponse(as, c, lib.skipSubjectCheck, response)
+  await lib.validateJwtAuthResponse(as, c, params)
 
-  jwt = await new jose.SignJWT({ sub: 'subject' })
+  jwt = await new jose.SignJWT({})
     .setProtectedHeader({ alg: 'ES256', kid: 'foo2' })
+    .setAudience(c.client_id)
+    .setExpirationTime('5m')
+    .setIssuer(as.issuer)
     .sign(key.privateKey)
 
-  response = getResponse(jwt, { headers: new Headers({ 'content-type': 'application/jwt' }) })
+  params = new URLSearchParams({ response: jwt })
 
   t.context
     .intercept({
@@ -108,15 +117,15 @@ test.serial('jwks_uri refetch if off cooldown and needed', async (t) => {
     })
 
   // re-fetch not allowed yet
-  await t.throwsAsync(() => lib.processUserInfoResponse(as, c, lib.skipSubjectCheck, response), {
+  await t.throwsAsync(() => lib.validateJwtAuthResponse(as, c, params), {
     message: 'error when selecting a JWT verification key, no applicable keys found',
   })
   timekeeper.travel(now + 59 * 1000)
-  await t.throwsAsync(() => lib.processUserInfoResponse(as, c, lib.skipSubjectCheck, response), {
+  await t.throwsAsync(() => lib.validateJwtAuthResponse(as, c, params), {
     message: 'error when selecting a JWT verification key, no applicable keys found',
   })
 
   timekeeper.travel(now + 60 * 1000)
 
-  await t.notThrowsAsync(() => lib.processUserInfoResponse(as, c, lib.skipSubjectCheck, response))
+  await t.notThrowsAsync(() => lib.validateJwtAuthResponse(as, c, params))
 })
