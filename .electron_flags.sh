5899994d37f41ba1f56ac0a7adccbf15fb2be37d
Filip Skokan <panva.ip@gmail.com>
2023-01-27 18:18:06 +0100

    test: update node and electron flags

--- .electron_flags.sh
+++ .electron_flags.sh
@@ -2,8 +2,8 @@ electron -i <<< 'process.exit(parseInt(process.versions.node, 10))' &> /dev/null
 NODE_VERSION=$?
 export NODE_OPTIONS='--no-warnings --tls-cipher-list="DHE-RSA-AES128-GCM-SHA256 ECDHE-RSA-AES128-GCM-SHA256 DHE-RSA-AES256-GCM-SHA384 ECDHE-RSA-AES256-GCM-SHA384"'
 
-if [[ $NODE_VERSION == 16 ]]; then
-  export NODE_OPTIONS+=' --experimental-global-webcrypto --experimental-fetch'
-elif [[ "$NODE_VERSION" == 18 ]]; then
+if [[ $NODE_VERSION -eq 18 ]]; then
   export NODE_OPTIONS+=' --experimental-global-webcrypto'
+elif [[ $NODE_VERSION -eq 16 ]]; then
+  export NODE_OPTIONS+=' --experimental-global-webcrypto --experimental-fetch'
 fi
--- .node_flags.sh
+++ .node_flags.sh
@@ -1,8 +1,9 @@
-NODE_VERSION=$(node -v)
+node -e 'process.exit(parseInt(process.versions.node, 10))' &> /dev/null
+NODE_VERSION=$?
 export NODE_OPTIONS='--conditions=browser --experimental-loader=@esbuild-kit/esm-loader --no-warnings --tls-cipher-list="DHE-RSA-AES128-GCM-SHA256 ECDHE-RSA-AES128-GCM-SHA256 DHE-RSA-AES256-GCM-SHA384 ECDHE-RSA-AES256-GCM-SHA384"'
 
-if [[ $NODE_VERSION == "v16."* ]]; then
-  export NODE_OPTIONS+=' --experimental-global-webcrypto --experimental-fetch'
-elif [[ "$NODE_VERSION" == "v18."* ]]; then
+if [[ $NODE_VERSION -eq 18 ]]; then
   export NODE_OPTIONS+=' --experimental-global-webcrypto'
+elif [[ $NODE_VERSION -eq 16 ]]; then
+  export NODE_OPTIONS+=' --experimental-global-webcrypto --experimental-fetch'
 fi
