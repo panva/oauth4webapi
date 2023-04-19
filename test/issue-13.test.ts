d7bf06118909c000f71210c92e19a404d945df09
Filip Skokan <panva.ip@gmail.com>
2023-04-19 12:02:22 +0200

    test: re-enable issue-13 test

--- test/issue-13.test.ts
+++ test/issue-13.test.ts
@@ -1,4 +1,9 @@
 // see https://github.com/panva/oauth4webapi/issues/13
+// All browser navigator user-agent strings start with Mozilla/5.0
+// It is unlikely to ever change too
+// @ts-expect-error
+globalThis.navigator = { userAgent: 'Mozilla/5.0 foo' }
+const lib = await import('../src/index.js')
 
 import anyTest, { type TestFn } from 'ava'
 import setup, { type Context, teardown, issuer, UA } from './_setup.js'
@@ -8,13 +13,7 @@ const test = anyTest as TestFn<Context>
 test.before(setup)
 test.after(teardown)
 
-test.skip('when in browser does not set custom user-agent', async (t) => {
-  // All browser navigator user-agent strings start with Mozilla/5.0
-  // It is unlikely to ever change too
-  // @ts-expect-error
-  globalThis.navigator = { userAgent: 'Mozilla/5.0 foo' }
-
-  const lib = await import('../src/index.js')
+test('when in browser does not set custom user-agent', async (t) => {
   const data = { ...issuer }
   t.context
     .intercept({
