651172181fc6b3e8c868079e0c938c1780bbde1a
Filip Skokan <panva.ip@gmail.com>
2023-08-27 17:43:08 +0200

    test: run webkit in github actions safari

--- tap/.browsers.sh
+++ tap/.browsers.sh
@@ -13,15 +13,6 @@ SSL=""
 
 if [[ -z $CI ]]; then
   BROWSER="chrome:headless"
-else
-  if [[ "$BROWSER" == "browserstack"* ]]; then
-    if [[ "$BROWSER" != "browserstack:android"* ]]; then
-      HOSTNAME="oauth4webapi.panva.me"
-      SSL="key=./letsencrypt/config/live/oauth4webapi.panva.me/privkey.pem;cert=./letsencrypt/config/live/oauth4webapi.panva.me/cert.pem;rejectUnauthorized=true;"
-    fi
-    BROWSER=$(NODE_PATH=$(npm root -g) node ./tap/browserstack.cjs $BROWSER)
-    echo "Using $BROWSER"
-  fi
 fi
 
 testcafe "$BROWSER" --skip-js-errors --ssl "$SSL" --hostname "$HOSTNAME" tap/.browser.ts
