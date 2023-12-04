34be94cceea240eae08eafaacddb551709232d52
Filip Skokan <panva.ip@gmail.com>
2023-12-04 13:02:50 +0100

    Revert "build: use tsx over esbuild-kit/esm-loader"
    This reverts commit 1a3dd65a6083987c83e2c9759df347f58ad9e9db.

--- .node_flags.sh
+++ .node_flags.sh
@@ -2,16 +2,10 @@ echo "Using Node.js $(node --version)"
 
 node -e 'process.exit(parseInt(process.versions.node, 10))' &> /dev/null
 NODE_VERSION=$?
-export NODE_OPTIONS='--conditions=browser --no-warnings --tls-cipher-list="DHE-RSA-AES128-GCM-SHA256 ECDHE-RSA-AES128-GCM-SHA256 DHE-RSA-AES256-GCM-SHA384 ECDHE-RSA-AES256-GCM-SHA384"'
+export NODE_OPTIONS='--conditions=browser --experimental-loader=@esbuild-kit/esm-loader --no-warnings --tls-cipher-list="DHE-RSA-AES128-GCM-SHA256 ECDHE-RSA-AES128-GCM-SHA256 DHE-RSA-AES256-GCM-SHA384 ECDHE-RSA-AES256-GCM-SHA384"'
 
 if [[ $NODE_VERSION -eq 18 ]]; then
   export NODE_OPTIONS+=' --experimental-global-webcrypto'
 elif [[ $NODE_VERSION -eq 16 ]]; then
   export NODE_OPTIONS+=' --experimental-global-webcrypto --experimental-fetch'
-fi
-
-if [[ $NODE_VERSION -lt 20 ]]; then
-  export NODE_OPTIONS+=' --loader=tsx/esm'
-else
-  export NODE_OPTIONS+=' --import=tsx/esm'
 fi
