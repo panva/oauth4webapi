30bf0f15bd273b6b3007c03f4e4767da9ecd0c70
Filip Skokan <panva.ip@gmail.com>
2024-01-12 11:41:46 +0100

    test: remove unnecessary workaround for iat-is-week-in-past

--- conformance/fapi/iat-is-week-in-past.ts
+++ conformance/fapi/iat-is-week-in-past.ts
@@ -1,14 +1,9 @@
 import { test, red, modules, variant } from '../runner.js'
 
 for (const module of modules('iat-is-week-in-past')) {
-  if (variant.fapi_response_mode === 'jarm') {
-    // TODO: https://gitlab.com/openid/conformance-suite/-/merge_requests/1368
-    test.todo('iat-is-week-in-past')
-  } else {
-    test.serial(
-      red(),
-      module,
-      'unexpected JWT "iat" (issued at) claim value, it is too far in the past',
-    )
-  }
+  test.serial(
+    red(),
+    module,
+    'unexpected JWT "iat" (issued at) claim value, it is too far in the past',
+  )
 }
