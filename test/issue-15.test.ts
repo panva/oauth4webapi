5cf5f0aef1282562da5ec3996c3f6afef394e76f
Filip Skokan <panva.ip@gmail.com>
2024-01-16 15:22:29 +0100

    test: fix test/issue-15.test.ts on node current

--- test/issue-15.test.ts
+++ test/issue-15.test.ts
@@ -4,17 +4,16 @@ import test from 'ava'
 import { issuer, client, getResponse } from './_setup.js'
 import * as lib from '../src/index.js'
 
-const response = getResponse(
-  JSON.stringify({ access_token: 'foo', token_type: 'bearer', scope: '' }),
-)
+const response = () =>
+  getResponse(JSON.stringify({ access_token: 'foo', token_type: 'bearer', scope: '' }))
 
 test('handles empty scope from the Token Endpoint', async (t) => {
   await t.notThrowsAsync(() =>
     Promise.all([
-      lib.processAuthorizationCodeOAuth2Response(issuer, client, response.clone()),
-      lib.processDeviceCodeResponse(issuer, client, response.clone()),
-      lib.processClientCredentialsResponse(issuer, client, response.clone()),
-      lib.processRefreshTokenResponse(issuer, client, response.clone()),
+      lib.processAuthorizationCodeOAuth2Response(issuer, client, response()),
+      lib.processDeviceCodeResponse(issuer, client, response()),
+      lib.processClientCredentialsResponse(issuer, client, response()),
+      lib.processRefreshTokenResponse(issuer, client, response()),
     ]),
   )
 })
