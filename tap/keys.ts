4a1959993242522e446022b51470c48eab55a9a4
Filip Skokan <panva.ip@gmail.com>
2025-06-06 12:34:26 +0200

    test: expect ed25519 support on blink

--- tap/keys.ts
+++ tap/keys.ts
@@ -1,13 +1,23 @@
 import * as lib from '../src/index.js'
 import * as env from './env.js'
 
-export const algs = ['PS256', 'PS384', 'PS512', 'RS256', 'RS384', 'RS512', 'ES256', 'ES384']
+export const algs = [
+  'PS256',
+  'PS384',
+  'PS512',
+  'RS256',
+  'RS384',
+  'RS512',
+  'ES256',
+  'ES384',
+  'EdDSA',
+  'Ed25519',
+]
 export const fails: string[] = []
 // TODO: remove when P-521 integration in Deno is finished
 if (!env.isDeno) {
   algs.push('ES512')
 }
-;(env.isBlink ? fails : algs).push('EdDSA', 'Ed25519')
 
 export const keys = algs.reduce(
   (acc, alg) => {
