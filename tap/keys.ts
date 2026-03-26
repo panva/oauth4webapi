f254402790195f3ee8ff98f0d2c6ce20cd167a48
Filip Skokan <panva.ip@gmail.com>
2026-03-26 09:57:34 +0100

    test: update deno expectations

--- tap/keys.ts
+++ tap/keys.ts
@@ -10,14 +10,11 @@ export const algs = [
   'RS512',
   'ES256',
   'ES384',
+  'ES512',
   'EdDSA',
   'Ed25519',
 ]
 export const fails: string[] = []
-// TODO: remove when P-521 integration in Deno is finished
-if (!env.isDeno) {
-  algs.push('ES512')
-}
 
 if (env.isNode && env.isNodeVersionAtLeast(24, 7)) {
   algs.push('ML-DSA-44', 'ML-DSA-65', 'ML-DSA-87')
