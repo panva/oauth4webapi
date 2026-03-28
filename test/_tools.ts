0b25c5c0c2de44e6a38b525a70206a83fcbe2b9b
Filip Skokan <panva.ip@gmail.com>
2026-03-28 12:47:13 +0100

    chore: bump packages

--- test/_tools.ts
+++ test/_tools.ts
@@ -3,7 +3,10 @@ import * as jose from 'jose'
 export function mangleJwtSignature(jwt: string) {
   const parts = jwt.split('.')
 
-  parts.push(jose.base64url.encode(crypto.getRandomValues(jose.base64url.decode(parts.pop()!))))
+  const decoded = jose.base64url.decode(parts.pop()!)
+  const random = new Uint8Array(decoded.length)
+  crypto.getRandomValues(random)
+  parts.push(jose.base64url.encode(random))
 
   return parts.join('.')
 }
